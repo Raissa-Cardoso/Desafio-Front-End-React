58f4622daa187a049395af1e03465284900defbc
Raissa Cardoso <86025905+Raissa-Cardoso@users.noreply.github.com>
2022-10-14 13:12:48 -0300

    updates progress and universities

--- desafio-frontend/src/Modal/Modal.jsx
+++ desafio-frontend/src/Modal/Modal.jsx
@@ -2,14 +2,14 @@ import './modal.css'
 import React,{useState,useEffect} from 'react'
 import dados from'../data/db.json'
 
-const logo=require("../assets/quero-Logo.jpg")
+//const logo=require("../assets/quero-Logo.jpg")
  
 
 export default function Modal(props){  
     let cidadeDif=[]
     let cidades=dados.map(cidade=>cidade.campus.city)
     cidades.map(cidade=>{
-        if(cidadeDif.indexOf(cidade)==-1){
+        if(cidadeDif.indexOf(cidade)===-1){
             cidadeDif.push(cidade)
         }
     })
@@ -17,11 +17,11 @@ export default function Modal(props){
     let cursosDif=[]
     let cursos=dados.map(curso=>curso.course.name)
     cursos.map(curso=>{
-        if(cursosDif.indexOf(curso)==-1){
+        if(cursosDif.indexOf(curso)===-1){
             cursosDif.push(curso)
         }
     })
-    let [cursosOp,setCursosOp]=useState(" ")  
+    let [cursosOp,setCursosOp]=useState(" ")      
     let [modalidade,setModalidade]=useState({Presencial:false,EaD:false})
     function selecionarModalidade(){  
         if(document.querySelector('#modPresencial').checked){
@@ -43,23 +43,24 @@ export default function Modal(props){
             }) 
         }
     }
-    function buscarOfertas(cidade, curso, presencial, ead){
+    function buscarOfertas(cidade, curso, presencial, ead, universidade){
         return{
             cidade:cidade,
             curso:curso,
             presencial:presencial,
-            ead:ead
+            ead:ead            
         }
     }
     let buscaOfertas=buscarOfertas(cidadesOp,cursosOp,modalidade.Presencial,modalidade.EaD)
-    let ofertas=dados.filter(oferta=>oferta.campus.city==cidadesOp)
-    if(Object.values(buscaOfertas)[0]!=" ") ofertas=ofertas.filter(oferta=>oferta.campus.city==cidadesOp)
-    if(Object.values(buscaOfertas)[1]!=" ") Object.values(buscaOfertas)[0]==" "?ofertas=dados.filter(oferta=>oferta.course.name==cursosOp):ofertas=ofertas.filter(oferta=>oferta.course.name==cursosOp) 
-    if(Object.values(buscaOfertas)[2]!=false&&Object.values(buscaOfertas)[3]!=true) ofertas.length==0?ofertas=dados.filter(oferta=>oferta.course.kind=="EaD"):ofertas=ofertas.filter(oferta=>oferta.course.kind=="EaD") 
-    if(Object.values(buscaOfertas)[3]!=false&&Object.values(buscaOfertas)[2]!=true) ofertas.length==0?ofertas=dados.filter(oferta=>oferta.course.kind=="Presencial"):ofertas=ofertas.filter(oferta=>oferta.course.kind=="Presencial")
-    if(Object.values(buscaOfertas)[2]==true&&Object.values(buscaOfertas)[3]==true) ofertas.length==0?ofertas=dados.filter(oferta=>oferta.course.kind=="Presencial"||oferta.course.kind=="EaD"):ofertas=ofertas.filter(oferta=>oferta.course.kind=="Presencial"||oferta.course.kind=="EaD")
-    if(Object.values(buscaOfertas)[0]==" "&&Object.values(buscaOfertas)[1]==" "&&Object.values(buscaOfertas)[2]==false&&Object.values(buscaOfertas)[3]==false)ofertas=dados.map(oferta=>oferta)
+    let ofertas=dados.filter(oferta=>oferta.campus.city===cidadesOp)
+    if(Object.values(buscaOfertas)[0]!==" ") ofertas=ofertas.filter(oferta=>oferta.campus.city===cidadesOp)
+    if(Object.values(buscaOfertas)[1]!==" ") Object.values(buscaOfertas)[0]===" "?ofertas=dados.filter(oferta=>oferta.course.name===cursosOp):ofertas=ofertas.filter(oferta=>oferta.course.name===cursosOp) 
+    if(Object.values(buscaOfertas)[2]!==false&&Object.values(buscaOfertas)[3]!==true) ofertas.length===0?ofertas=dados.filter(oferta=>oferta.course.kind==="EaD"):ofertas=ofertas.filter(oferta=>oferta.course.kind==="EaD") 
+    if(Object.values(buscaOfertas)[3]!==false&&Object.values(buscaOfertas)[2]!==true) ofertas.length===0?ofertas=dados.filter(oferta=>oferta.course.kind==="Presencial"):ofertas=ofertas.filter(oferta=>oferta.course.kind==="Presencial")
+    if(Object.values(buscaOfertas)[2]===true&&Object.values(buscaOfertas)[3]===true) ofertas.length===0?ofertas=dados.filter(oferta=>oferta.course.kind==="Presencial"||oferta.course.kind==="EaD"):ofertas=ofertas.filter(oferta=>oferta.course.kind==="Presencial"||oferta.course.kind==="EaD")
+    if(Object.values(buscaOfertas)[0]===" "&&Object.values(buscaOfertas)[1]===" "&&Object.values(buscaOfertas)[2]===false&&Object.values(buscaOfertas)[3]===false)ofertas=dados.map(oferta=>oferta)
     
+
     let ofertasOrdenadas=ofertas.sort(function (a, b) {
         if (a.course.name > b.course.name) {
           return 1;
@@ -68,8 +69,16 @@ export default function Modal(props){
           return -1;
         }        
         return 0;
-      })    
-    let [progressoClick, setProgressoClick]=useState(true)
+      })
+    let universidadeDif=[]
+    let universidades=ofertasOrdenadas.map(oferta=>oferta.university.name)
+    universidades.map(universidade=>{
+        if(universidadeDif.indexOf(universidade)===-1){
+            universidadeDif.push(universidade)
+        }
+    })
+    let [universidadeOp,setUniversidadeOp]=useState(universidadeDif.sort()[0])         
+    //let [progressoClick, setProgressoClick]=useState(true)
     let [progressoBotao,setProgressoBotao]=useState(document.querySelector('.progressoBotao'))       
     function fecharModal(){
        props.fechar()           
@@ -78,19 +87,39 @@ export default function Modal(props){
         //setProgressoBotao((state) => progressoClick?document.querySelector('.progressoBotao').style.marginLeft : document.querySelector('.progressoBotao').style.marginLeft='calc(+5px)');
         //setProgressoBotao((state) => progressoClick?document.querySelector('.progressoBotao').style.transform='translateX(5px)' : document.querySelector('.progressoBotao').style.transform='translateX(5px)');
         
-    },[progressoBotao]);    
-    function clicarProgressoBotao(){
-        setProgressoClick(state => !state)
-        setProgressoBotao(document.querySelector('.progressoBotao'))        
-        //progressoBotao.style.marginLeft='calc(+5px)'        
-        //progressoBotao.style.transition='progresso-animacao 5s infinite'
-        //progressoBotao.style.transformn='translateX(10px)'        
+    },[progressoBotao]);
+    let[contador,setContador]=useState(0)
+    function clicarProgressoBotao(){        
+        //setProgressoClick(state => !state)
+        /*document.addEventListener('mouseEvent.clientX', function(ev){     
+            console.log("ok")
+        },false);*/
+        if(contador<20)
+        {
+            setContador(contador+1)
+            setProgressoBotao(document.querySelector('.progressoBotao').style.transform += 'translateX('+(13)+'px)')
+            document.addEventListener('click', function(ev){     
+                progressoBotao.style.transform += 'translateX('+(13)+'px)'; 
+            },false); 
+        }
+        /*if(contador>=20) {            
+            setProgressoBotao(document.querySelector('.progressoBotao').style.transform += 'translateX('+(-10)+'px)')
+            document.addEventListener('click', function(ev){     
+                progressoBotao.style.transform += 'translateX('+(-10)+'px)'; 
+            },false); 
+            
+        }*/
+          
     }
     function alterarCidade(){        
         setCidadesOp(document.querySelector('#cidade').value)            
     }
     function alterarCurso(){              
         setCursosOp(document.querySelector('#curso').value)            
+    }
+    function alterarUniversidade(){
+        setUniversidadeOp(document.querySelector('#universidadeOp').value)        
+        ofertas=ofertas.filter(oferta=>oferta.university.name===universidadeOp)       
     }
 
     return(
@@ -136,7 +165,7 @@ export default function Modal(props){
                 </div>                
                 <div className='preçoModal'>
                     <p>ATÉ QUANTO PODE PAGAR?</p>
-                    <p>R$10.000</p>
+                    <p>R${contador*500}</p>
                     <div className="linhaProgresso">
                         <button onClick={clicarProgressoBotao} className="progressoBotao"></button>
                     </div>
@@ -148,7 +177,14 @@ export default function Modal(props){
                 </div>                
                 <div className='ordenar'>
                     <p>Ordenar por </p>
-                    <a href=''>Nome da Faculdade</a>
+                    <select name="universidadeOp" id="universidadeOp"  onChange={alterarUniversidade}>   
+                        <option value="Nome da faculdade">Nome da faculdade</option>                     
+                        {                          
+                            universidadeDif.sort().map((universidade,index)=>
+                                <option key={index} value={universidade}>{universidade}</option>
+                            )
+                        }      
+                    </select>                    
                 </div>    
             </div>
             <div className="divisao"/> 
